701514e7222575899d952438896d5eada89a7b73
Ratmir Gaitov <gaitovrat@gmail.com>
2023-03-26 19:14:04 +0200

    Reimplement follow

--- src/main/java/vsb/gai0010/grammar/GrammarOperations.java
+++ src/main/java/vsb/gai0010/grammar/GrammarOperations.java
@@ -16,25 +16,33 @@ public class GrammarOperations {
 	private final Terminal empty;
 	private final Terminal dollar;
 	private final Table<Symbol, Boolean> firstTable;
+	private final Table<Symbol, Boolean> followTable;
 	private boolean isFirstTableFilled;
+	private boolean isFollowTableFilled;
 
     public GrammarOperations(Grammar grammar) {
         this.grammar = grammar;
         this.empty = new Terminal("{e}");
         this.dollar = new Terminal("{$}");
         this.firstTable = new Table<>(false);
+        this.followTable = new Table<>(false);
         this.emptyNonterminals = new TreeSet<>();
         this.isFirstTableFilled = false;
+        this.isFollowTableFilled = false;
         
         computeEmpty();
         
         for (Nonterminal nonterminal : grammar.getNonterminals()) {
         	this.firstTable.addRow(nonterminal);
         	this.firstTable.addColumn(nonterminal);
+        	this.followTable.addRow(nonterminal);
+        	this.followTable.addColumn(nonterminal);
         }
         for (Terminal terminal : grammar.getTerminals()) {
         	this.firstTable.addColumn(terminal);
+        	this.followTable.addColumn(terminal);
         }
+        this.followTable.addColumn(dollar);
     }
 
     public Set<Nonterminal> getEmptyNonterminals() {
@@ -86,8 +94,6 @@ public class GrammarOperations {
     		return;
     	}
     	
-    	boolean hasChanged = false;
-    	
     	for (Rule rule : this.grammar.getRules()) {
     		Nonterminal lhs = rule.getLHS();
     		List<Symbol> rhs = rule.getRHS();
@@ -111,18 +117,24 @@ public class GrammarOperations {
     		}
     	}
     	
+    	copyTerminalValues(this.firstTable);
+    }
+    
+    private static void copyTerminalValues(Table<Symbol, Boolean> table) {
+    	boolean hasChanged = false;
+    	
     	do {
     		hasChanged = false;
     		
-    		for (Symbol row : this.firstTable.getRowNames()) {
-    			for (Symbol col : this.firstTable.getColumnNames()) {
-    				boolean value = this.firstTable.getCell(row, col); 
+    		for (Symbol row : table.getRowNames()) {
+    			for (Symbol col : table.getColumnNames()) {
+    				boolean value = table.getCell(row, col); 
     				
     				if (value == true && col instanceof Nonterminal) {
-    					for (Symbol colCopy : this.firstTable.getColumnNames()) {
-    						if (this.firstTable.getCell(row, colCopy) == false && 
-    								this.firstTable.getCell(col, colCopy) == true) {
-    							this.firstTable.setCell(row, colCopy, true);
+    					for (Symbol colCopy : table.getColumnNames()) {
+    						if (table.getCell(row, colCopy) == false && 
+    								table.getCell(col, colCopy) == true) {
+    							table.setCell(row, colCopy, true);
     							hasChanged = true;
     						}
     					}
@@ -130,6 +142,59 @@ public class GrammarOperations {
     			}
     		}
     	} while(hasChanged);
+    }
+    
+    public Set<Terminal> first(List<Symbol> symbols) {
+    	Set<Terminal> result = new TreeSet<>();
+    	
+    	if (symbols.isEmpty()) {
+    		result.add(empty);
+			return result;
+		}
+		
+		if (symbols.get(0) instanceof Terminal) {
+			result.add((Terminal) symbols.get(0));
+			return result;
+		}
+		
+		if (this.emptyNonterminals.contains(symbols.get(0)) == false) {
+			Symbol row = symbols.get(0);
+			List<Symbol> columns = this.firstTable.getColumnNames()
+					.stream()
+					.filter(el -> el instanceof Terminal)
+					.toList();
+			for (Symbol col : columns) {
+				if (this.firstTable.getCell(row, col) == true) {
+					result.add((Terminal) col);
+				}
+			}
+			return result;
+		}
+		
+		if (symbols.stream().filter(el -> this.emptyNonterminals.contains(el) == false).count() == 0) {
+			result.add(empty);
+		}
+		
+		for (Symbol symbol : symbols) {
+			Symbol row = symbols.get(0);
+			
+			if (symbol instanceof Terminal) {
+				result.add((Terminal) symbol);
+				continue;
+			}
+			
+			List<Symbol> columns = this.firstTable.getColumnNames()
+					.stream()
+					.filter(el -> el instanceof Terminal)
+					.toList();
+			for (Symbol col : columns) {
+				if (this.firstTable.getCell(row, col) == true) {
+					result.add((Terminal) col);
+				}
+			}
+		}
+		
+		return result;
     }
     
     public Map<String, Set<Terminal>> first() {
@@ -142,66 +207,58 @@ public class GrammarOperations {
 				.stream()
 				.map(el -> el.toString())
 				.reduce("", (el1, el2) -> el1 + el2);
-    		Set<Terminal> terminals = new TreeSet<>();
 
     		List<Symbol> rhs = rule.getRHS();
     		
-    		if (rhs.isEmpty()) {
-    			terminals.add(empty);
-    			result.put(key, terminals);
-    			continue;
-    		}
-    		
-    		if (rhs.get(0) instanceof Terminal) {
-    			terminals.add((Terminal) rhs.get(0));
-    			result.put(key, terminals);
-    			continue;
-    		}
-    		
-    		if (this.emptyNonterminals.contains(rhs.get(0)) == false) {
-    			Symbol row = rhs.get(0);
-    			List<Symbol> columns = this.firstTable.getColumnNames()
-    					.stream()
-    					.filter(el -> el instanceof Terminal)
-    					.toList();
-    			for (Symbol col : columns) {
-    				if (this.firstTable.getCell(row, col) == true) {
-    					terminals.add((Terminal) col);
-    				}
-    			}
-    			result.put(key, terminals);
-    			continue;
-    		}
-    		
-    		if (rhs.stream().filter(el -> this.emptyNonterminals.contains(el) == false).count() == 0) {
-    			terminals.add(empty);
-    		}
-    		
-    		for (Symbol symbol : rhs) {
-    			Symbol row = rhs.get(0);
-    			if (symbol instanceof Terminal) {
-    				terminals.add((Terminal) symbol);
-    				continue;
-    			}
-    			
-    			List<Symbol> columns = this.firstTable.getColumnNames()
-    					.stream()
-    					.filter(el -> el instanceof Terminal)
-    					.toList();
-    			for (Symbol col : columns) {
-    				if (this.firstTable.getCell(row, col) == true) {
-    					terminals.add((Terminal) col);
-    				}
-    			}
-    		}
-    		result.put(key, terminals);
+    		result.put(key, first(rhs));
     	}
     	
     	return result;
     }
     
+    private void fillFollowTable() {
+    	if (this.isFollowTableFilled) {
+    		return;
+    	}
+    	
+    	this.fillFirstTable();
+    	
+    	this.followTable.setCell(this.grammar.getStartNonterminal(), this.dollar, true);
+    	
+    	for (Rule rule : this.grammar.getRules()) {
+    		Nonterminal lhs = rule.getLHS();
+    		List<Symbol> rhs = rule.getRHS();
+    		
+    		for (int i = 0; i < rhs.size(); i++) {
+    			if (rhs.get(i) instanceof Nonterminal) {
+					Set<Terminal> terminals = first(rhs.subList(i + 1, rhs.size()));
+					terminals.remove(empty);
+					
+					if (terminals.isEmpty()) {
+						this.followTable.setCell(rhs.get(i), lhs, true);
+					}
+					for (Terminal terminal : terminals) {
+						this.followTable.setCell(rhs.get(i), terminal, true);
+					}
+    			}
+    		}
+    	}
+    	
+    	copyTerminalValues(this.followTable);
+    	
+    	this.isFollowTableFilled = true;
+    }
+    
     public Set<Terminal> follow(Nonterminal nonterminal) {
     	Set<Terminal> result = new TreeSet<>();
+    	
+    	this.fillFollowTable();
+    	
+    	for (Symbol symbol : this.followTable.getColumnNames()) {
+    		if (symbol instanceof Terminal && this.followTable.getCell(nonterminal, symbol) == true) {
+    			result.add((Terminal) symbol);
+    		}
+    	}
     	
     	return result;
     }
@@ -210,11 +267,7 @@ public class GrammarOperations {
     	Map<Nonterminal, Set<Terminal>> result = new TreeMap<>();
     	
     	for (Nonterminal nonterminal : this.grammar.getNonterminals()) {
-    		result.put(nonterminal, new TreeSet<>());
-    	}
-    	
-    	for (Entry<Nonterminal, Set<Terminal>> entry : result.entrySet()) {
-    		result.get(entry.getKey()).addAll(follow(entry.getKey()));
+    		result.put(nonterminal, follow(nonterminal));
     	}
     	
     	return result;
